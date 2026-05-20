f8cfa1f8f91049f074524bbc05041283f2a8fd5e
Your Name <brightsy04@gmail.com>
2026-05-20 01:49:53 -0700

    20min

--- main.cpp
+++ main.cpp
@@ -7,5 +7,31 @@ using namespace std;
 
 int main()
 {
+    map<string, int> airportTraffic;
+    ifstream inputFile("210-final-1-SP26");
+
+    if (!inputFile)
+    {
+        cout << "Error, could not open" << endl;
+        return 1;
+    }
+
+    string origin;
+    string destination;
+
+    while (inputFile >> origin >> destination){
+        if (airportTraffic.find(origin) == airportTraffic.end())
+        {
+            airportTraffic[origin] = 0;
+        }
+
+        if (airportTraffic.find(destination) == airportTraffic.end())
+        {
+            airportTraffic[destination] = 0;
+        }
+
+    }
+
+
     return 0;
 }
